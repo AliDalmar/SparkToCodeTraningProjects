dea43c5f6f000c4b6244df3cd55f2ff34ef21533
Ali <Ali@LAPTOP-Q4018NF0>
2026-06-27 18:05:08 +0400

    add first project files

---
+++ FirstC#Project/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstC_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
